4e51dbb785b4d901d6d414530cb69eb943a7ec45
sasan <sasan@DESKTOP-2VR36R0>
2018-01-14 11:42:54 -0600

    Custom login for security page

--- spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -14,7 +14,14 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
+		
+		http.authorizeRequests().anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/showMyLoginPage")
+		.loginProcessingUrl("/authenticateTheUser")
+		.permitAll();
+		
 		super.configure(http);
 	}
 
